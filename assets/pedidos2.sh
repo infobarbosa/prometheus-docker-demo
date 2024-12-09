9bc5c0de7c0ebae4557f74b24fa206cccdc01df9
infobarbosa <infobarbosa@yahoo.com.br>
2024-12-09 16:45:50 -0300

    update pedidos2.sh to change metrics endpoint from 9091 to 9090

--- assets/pedidos2.sh
+++ assets/pedidos2.sh
@@ -3,9 +3,9 @@ produtos=("TV" "GELADEIRA" "TV" "HOMETHEATER" "COMPUTADOR" "MONITOR" "TABLET" "S
 while(true)
 do
     if [ $(expr $i % 2 ) != "0" ]; then
-        echo "Quantity ``${RANDOM:0:2}``" | curl --data-binary @- http://localhost:9091/metrics/job/pedidos/pais/BR/produto/${produtos[${RANDOM:0:1}]}
+        echo "Quantity ``${RANDOM:0:2}``" | curl --data-binary @- http://localhost:9090/metrics/job/pedidos/pais/BR/produto/${produtos[${RANDOM:0:1}]}
     else
-        echo "Quantity ``${RANDOM:0:2}``" | curl --data-binary @- http://localhost:9091/metrics/job/pedidos/pais/US/produto/${produtos[${RANDOM:0:1}]}
+        echo "Quantity ``${RANDOM:0:2}``" | curl --data-binary @- http://localhost:9090/metrics/job/pedidos/pais/US/produto/${produtos[${RANDOM:0:1}]}
     fi
     sleep 0.05
 done
